e552fe8086d31bdbbeb2a8f5c5c56dce022e61a6
Corey <hamhams86@gmail.com>
2022-08-25 14:50:47 -0700

    added procfile

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
     FLASK_APP = os.getenv('FLASK_APP')
-    FLASK_ENV = os.getenv('FLASK_ENV')
+    FLASK_RUN = os.getenv('FLASK_RUN')
     SECRET_KEY = os.environ.get('CT_SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
     SQLALCHEMY_TRACK_NOTIFICATIONS = False
